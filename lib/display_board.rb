976bb95e33cc8855658c87454f3ae7cce9c4d392
Matt Boecker <matt.boecker@becktronics.org>
2018-04-07 03:17:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+rows = ["   |   |   ","-----------","   |   |   ","-----------","   |   |   "]
+
+puts "#{rows[0]}"
+puts "#{rows[1]}"
+puts "#{rows[2]}"
+puts "#{rows[3]}"
+puts "#{rows[4]}"
+board[0] = "O"
+end
