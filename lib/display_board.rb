aa220d6d2d3ce4b6713882d0c62120e621fe073a
Matt Boecker <matt.boecker@becktronics.org>
2018-04-07 04:37:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,8 +8,4 @@ puts "#{rows[2]}"
 puts "#{rows[3]}"
 puts "#{rows[4]}"
 
-board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
-board = [" "," "," "," ","X"," "," "," "," "]
-display_board(board)
 end
