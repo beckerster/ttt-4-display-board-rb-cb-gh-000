76c55952aa0973867e51db90d7d7198ff15e160d
Matt Boecker <matt.boecker@becktronics.org>
2018-04-07 04:13:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 def display_board(board)
-capture_puts{ display_board(board)
+capture_puts{ display_board(board)}
 rows = ["   |   |   ","-----------","   |   |   ","-----------","   |   |   "]
 
 puts "#{rows[0]}"
