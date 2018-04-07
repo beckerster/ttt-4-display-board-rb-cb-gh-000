ecab1ae7d4c7b02aff17aebc91611c2b7e6c7c8c
Matt Boecker <matt.boecker@becktronics.org>
2018-04-07 03:33:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 def display_board(board)
 rows = ["   |   |   ","-----------","   |   |   ","-----------","   |   |   "]
-
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 puts "#{rows[0]}"
 puts "#{rows[1]}"
 puts "#{rows[2]}"
 puts "#{rows[3]}"
 puts "#{rows[4]}"
-board[0] = "O"
+
 end
