4ea538e7bdbdc1cd0617a5f1a19e0a1c37399218
Matt Boecker <matt.boecker@becktronics.org>
2018-04-07 04:07:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,4 +9,15 @@ puts "#{rows[3]}"
 puts "#{rows[4]}"
 
 end
-def 
+
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  rows = ["   |   |   ","-----------","   |   |   ","-----------","   |   |   "]
+
+  puts "#{rows[0]}"
+  puts "#{rows[1]}"
+  puts "#{rows[2]}"
+  puts "#{rows[3]}"
+  puts "#{rows[4]}"
+
+  end
